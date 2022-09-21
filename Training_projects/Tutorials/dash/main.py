b07db683a8ba4d33843813e2057bb6f80c78ebc3
sccasutt <71444392+sccasutt@users.noreply.github.com>
2022-09-21 14:03:09 +0200

    More callbacks

--- Training_projects/Tutorials/dash/main.py
+++ Training_projects/Tutorials/dash/main.py
@@ -26,13 +26,57 @@ app = Dash(__name__,external_stylesheets=external_stylesheets)
 app.layout = html.Div(children=[
                                 html.H1(children='Total Traffic Accidents in Switzerland by Year'),
                                 html.H5(children='Interactive Visualization Tutorial'),
-                                html.Div(children=[dcc.RangeSlider(int(line_df['Year'].min()), int(line_df['Year'].max()), 
-                                                                    marks={str(year): str(year) for year in line_df['Year'].unique()},
-                                                                    value=[int(line_df['Year'].min())+10,int(line_df['Year'].max())-10], 
-                                                                    id='year_selector')]),
-                                html.Div(children=[dcc.Graph(id='accidents_graphic')]),
                                 
-                     ])
+                                ### Top half of the app
+                                html.Div(children=[
+                                                    
+                                    html.Div(children=[
+                                                                        
+                                        html.Br(),
+                                         #left side of the top half                               
+                                        html.Div(children=[
+                                            html.H6('Select a range of years to inspect:'),
+
+                                            html.Br(),
+
+                                            #Year range selector
+                                            dcc.RangeSlider(int(line_df['Year'].min()), int(line_df['Year'].max()), 
+                                                            marks={str(year): str(year) for year in line_df['Year'].unique()},
+                                                            value=[int(line_df['Year'].min())+10,int(line_df['Year'].max())-10],  
+                                                            id='year_selector', 
+                                                            ),
+                                                                                            
+                                            #graph to be displayed                                               
+                                            dcc.Graph(id='accidents_graphic')
+                                                                                                                
+                                            #don't fill the screen                                                         
+                                            ],style={'width': '60%',
+                                                     'display': 'inline-block', 
+                                                     'vertical-align': 'top'} 
+                                            ), 
+                                    #Right side of the top half
+                                    html.Div(children=[
+                                        html.H3('Key insights:'),
+                                        html.H5(id='delta_percent'),
+                                        html.H6(id='delta_by_accident_type'),
+                                        html.H6(id='delta_dy_road_type'),
+
+
+                                    ],style={'width': '30%',
+                                             'display': 'inline-block', 
+                                             'vertical-align': 'top',
+                                             "margin-left": "3rem", 
+                                             'margin-top':'14rem'})  
+                                                                                            
+                                        ],style={'width': '100%',
+                                                 'height':'50%', 
+                                                 'display': 'inline-block', 
+                                                 'vertical-align': 'top'}  
+                                        )
+                                ])
+                ])
+
+                                   
 
 #create callbacks-----------------------------------------------
 
@@ -44,6 +88,42 @@ def update_figure(year_range):
     fig = px.line(updated_df, x='Year', y='Amount')
     fig.update_layout(transition_duration=500)
     return fig
+
+
+@app.callback(Output('delta_percent','children'),
+              Input('year_selector','value'))
+
+def update_delta(year_range):
+    start_total = line_df[line_df['Year'] == int(year_range[0])]['Amount'].values[0]
+    end_total = line_df[line_df['Year'] == int(year_range[-1])]['Amount'].values[0]
+    delta_percent = 100-(start_total/end_total)*100
+    return f'From {year_range[0]:.0f} to {year_range[-1]:.0f} the total number of accidents changed by {delta_percent:.2f}%.'
+
+
+@app.callback(Output('delta_by_accident_type','children'),
+              Input('year_selector','value'))
+
+def update_delta_acc_type(year_range):
+    acc_df = pd.DataFrame(df.groupby(['Unfallschwere','Year'])['Amount'].sum())
+    acc_df.reset_index(inplace=True)
+    acc_df['Year'] = pd.to_numeric(acc_df['Year'])
+    start_total = acc_df[acc_df['Year'] == int(year_range[0])]['Amount'].values
+    end_total = acc_df[acc_df['Year'] == int(year_range[-1])]['Amount'].values
+    delta_percent = 100-(start_total/end_total)*100
+    return f'Deadly accidents changed by {delta_percent[0]:.2f}%, Accidents with serious bodly harm changed by {delta_percent[1]:.2f}% and non serious accidents changed by {delta_percent[2]:.2f}%.'
+
+@app.callback(Output('delta_dy_road_type','children'),
+              Input('year_selector','value'))
+
+def update_delta_acc_type(year_range):
+    road_df = pd.DataFrame(df.groupby(['Strassenart','Year'])['Amount'].sum())
+    road_df.reset_index(inplace=True)
+    road_df['Year'] = pd.to_numeric(road_df['Year'])
+    road_df_update = road_df[road_df['Year'].between(year_range[0],year_range[-1])]
+    max_acc_road = road_df_update['Amount'].values.max()
+    road_type = road_df_update[road_df_update['Amount'] == road_df_update['Amount'].max()]['Strassenart'].values[0]
+    max_year = road_df_update[road_df_update['Amount'] == road_df_update['Amount'].max()]['Year'].values[0]
+    return f'Most accidents happend on {road_type} with {max_acc_road} accidents in {max_year}.'
 
 #Run app---------------------------------------------
 
